c441db36c716340b7e189525eaebdb00931cca2a
Adnan Issadeen <eddyrox1@hotmail.com>
2017-12-15 12:17:36 +0530

    Completed day 1 part 2

--- python/day1solution.py
+++ python/day1solution.py
@@ -14,10 +14,34 @@ def solve_captcha(captcha_string):
         i+=1
     return sum(values_to_add)
 
-def solve_day_1_part_1_challenge_1():
+def solve_captcha_part_two(captcha_string):
+    halfway_length = len(captcha_string)//2
+    input_length = len(captcha_string)
+    values_to_add = []
+    i = 0
+    while i<input_length:
+        current_value = captcha_string[i]
+        if i+halfway_length>=input_length:
+            next_value = captcha_string[i + halfway_length - input_length]
+        else:
+            next_value = captcha_string[i + halfway_length]
+        if current_value == next_value:
+            values_to_add.append(int(current_value))
+        i+=1
+    return sum(values_to_add)
+
+def solve_challenge_part_1():
     puzzle_input_file = open('python/day1part1input.txt', 'r')
     puzzle_input = puzzle_input_file.read()
     print(solve_captcha(puzzle_input.strip()))
 
+def solve_challenge_part_2():
+    puzzle_input_file = open('python/day1part1input.txt', 'r')
+    puzzle_input = puzzle_input_file.read()
+    print(solve_captcha_part_two(puzzle_input.strip()))
+
 if __name__ == "__main__":
-    solve_day_1_part_1_challenge_1()
+    print("=== Part 1 ===")
+    solve_challenge_part_1()
+    print("=== Part 2 ===")
+    solve_challenge_part_2()
--- python/day1test.py
+++ python/day1test.py
@@ -1,7 +1,7 @@
 import unittest
 from . import day1solution as solution
 
-class TestDay1Solution(unittest.TestCase):
+class TestDay1Part1Solution(unittest.TestCase):
     def test_1122_returns_3(self):
         self.assertEqual(solution.solve_captcha("1122"), 3, "captcha for 1122 should return 3")
     def test_1111_returns_4(self):
@@ -11,6 +11,21 @@ class TestDay1Solution(unittest.TestCase):
     def test_91212129_returns_9(self):
         self.assertEqual(solution.solve_captcha("91212129"), 9)
 
+class TestDay1Part2Solution(unittest.TestCase):
+    def test_1212_returns_6(self):
+        self.assertEqual(solution.solve_captcha_part_two("1212"), 6)
+
+    def test_1221_returns_0(self):
+        self.assertEqual(solution.solve_captcha_part_two("1221"), 0)
+
+    def test_123425_returns_4(self):
+        self.assertEqual(solution.solve_captcha_part_two("123425"), 4)
+
+    def test_123123_returns_12(self):
+        self.assertEqual(solution.solve_captcha_part_two("123123"), 12)
+
+    def test_12131415_returns_4(self):
+        self.assertEqual(solution.solve_captcha_part_two("12131415"), 4)
 
 if __name__ == "__main__":
     unittest.main()
